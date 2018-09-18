3741a01bff4aa1d00ea01e60e0275befebb1dd09
DragonicNick <nickoorthuizen@gmail.com>
2018-09-18 15:29:01 +0200

    Card prefab remade

---
+++ Unity/Assets/Scripts/Card.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+
+    public int id;
+    public string name;
+    public string description;
+    public string picturePath;
+    public int cost;
+
+
+	void Start () {
+		//todo: api call to get card info
+        //todo: put card info on card
+	}
+
+	void FixedUpdate () {
+		
+	}
+}
