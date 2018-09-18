5e98ad997fbc29523fa4d4ad7fbfb72421e26268
BobvanVelzen <bob.vanvelzen@student.fontys.nl>
2018-09-18 14:49:55 +0200

    Drag & Drop + Abstract Effect Class
    Added drap and drop for ui elements
    Abstract Effect class added

---
+++ Unity/Assets/Scripts/Effect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Effect : ScriptableObject {
+    
+    public int speed;
+
+    public virtual string getDescription()
+    {
+        return "";
+    }
+
+    public virtual void Apply() { }
+}
