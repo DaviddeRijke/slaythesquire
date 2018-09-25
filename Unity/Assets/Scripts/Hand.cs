a3759dd45874cf0a47e88f7f429c78fc1ab2b6c5
BobvanVelzen <bob.vanvelzen@student.fontys.nl>
2018-09-25 13:42:33 +0200

    Hand Fit
    Kaarten worden in de hand gedisplayed

--- Unity/Assets/Scripts/Hand.cs
+++ Unity/Assets/Scripts/Hand.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Hand : MonoBehaviour {
 
-    private List<GameObject> cards; 
+    private List<GameObject> cards;
+    public float spacing = 0.6f;
+    public float distance = 0.03f;
 
 	// Use this for initialization
 	void Start () {
+        cards = new List<GameObject>();
+        UpdateList();
+        FitCards();
+	}
+
+    private void UpdateList()
+    {
+        cards.Clear();
         foreach (Transform t in transform)
         {
             GameObject card = t.gameObject;
@@ -16,10 +26,32 @@ public class Hand : MonoBehaviour {
                 cards.Add(card);
             }
         }
+    }
+
+	public void FitCards() {
+        if (cards.Count == 0)
+            return;
+
+        for (int i = 0; i < cards.Count; i++)
+        {
+            Transform card = cards[i].transform;
+
+            card.localPosition = new Vector3(transform.position.x - (spacing * cards.Count * 0.5f - spacing * 0.5f), 0f, 0f);
+            card.localPosition += new Vector3(i * spacing, distance * i, 0f);
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void AddCard(GameObject card)
+    {
+        cards.Add(card);
+        card.transform.SetParent(transform);
+        FitCards();
+    }
+
+    public void RemoveCard(GameObject card)
+    {
+        cards.Remove(card);
+        card.transform.SetParent(null);
+        FitCards();
+    }
 }
