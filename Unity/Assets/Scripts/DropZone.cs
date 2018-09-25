3df4d498003545a0688cc1ac1325c285d33d5811
BobvanVelzen <bob.vanvelzen@student.fontys.nl>
2018-09-25 14:06:48 +0200

    DropZone update
    removed enum DropZoneType

--- Unity/Assets/Scripts/DropZone.cs
+++ Unity/Assets/Scripts/DropZone.cs
@@ -3,37 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ZoneType
-{
-    DISCARD_PILE,
-    EQUIPMENT_SLOT,
-    FIELD,
-}
 
 
 public class DropZone : MonoBehaviour {
 
-    public ZoneType type;
-
     public void DropCard(Card card)
     {
-        // TODO: Check if Card can be activated
-        // TODO: Return Card to hand if cannot be activated
-        switch (type)
-        {
-            case ZoneType.DISCARD_PILE:
-                // Discard Card
-                break;
-            case ZoneType.EQUIPMENT_SLOT:
-                // Equip Card
-                // This can change to Field where Equip Cards will be played to equip an item
-                break;
-            case ZoneType.FIELD:
-                PlayCard(card);
-                break;
-            default:
-                break;
-        }
+        PlayCard(card);
     }
 
     void PlayCard(Card card)
