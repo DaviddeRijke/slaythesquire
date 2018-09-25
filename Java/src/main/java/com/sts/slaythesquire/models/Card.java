046fd6cef5aa26738841d90f57ba02459632c853
David <davidpieter98@live.nl>
2018-09-25 11:53:44 +0200

    Domeinmodel implemented

--- Java/src/main/java/com/sts/slaythesquire/models/Card.java
+++ Java/src/main/java/com/sts/slaythesquire/models/Card.java
@@ -1,9 +1,7 @@
 package com.sts.slaythesquire.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Collection;
 
 @Entity
 public class Card {
@@ -11,22 +9,41 @@ public class Card {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-
     private String name;
+
+    @ManyToMany(cascade=CascadeType.ALL)
+    @JoinTable(name="card_tag", joinColumns = @JoinColumn(name="card_id"), inverseJoinColumns = @JoinColumn(name="tag_id"))
+    private Collection<Tag> tags;
+
+    @ManyToMany(cascade=CascadeType.ALL)
+    @JoinTable(name="card_tag", joinColumns = @JoinColumn(name="card_id"), inverseJoinColumns = @JoinColumn(name="effect_id"))
+    private Collection<Effect> effects;
+    private EndTurnAction endTurnAction;
 
     public void setId(final int id){
         this.id = id;
     }
-
     public int getId(){
         return id;
     }
 
-    public void setName(final String name){
-        this.name = name;
-    }
-
+    public void setName(final String name){this.name = name;}
     public String getName(){
         return name;
     }
+
+    public void setTags(final Collection<Tag> tags){ this.tags = tags; }
+    public Collection<Tag> getTags(){return tags;}
+
+    public void addTag(Tag tag){if(!tags.contains(tag)) tags.add(tag);}
+    public void removeTag(Tag tag){ tags.remove(tag);}
+
+    public void setEffects(final Collection<Effect> effects){ this.effects = effects; }
+    public Collection<Effect> getEffects(){return effects;}
+
+    public void addEffect(Effect effect){if(!effects.contains(effect)) effects.add(effect);}
+    public void removeEffect(Effect effect){ effects.remove(effect);}
+
+    public void setEndTurnAction(EndTurnAction endTurnAction){this.endTurnAction = endTurnAction;}
+    public EndTurnAction getEndTurnAction(){return endTurnAction;}
 }
---
+++ Java/src/main/java/com/sts/slaythesquire/models/EndTurnAction.java
@@ -0,0 +1,24 @@
+package com.sts.slaythesquire.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class EndTurnAction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+
+    public void setId(final int id){
+        this.id = id;
+    }
+    public int getId(){
+        return id;
+    }
+
+    public void setName(final String name){this.name = name;}
+    public String getName(){
+        return name;
+    }
+}
