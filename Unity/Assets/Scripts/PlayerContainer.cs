37bb7a8f0394d992eb2b570a27c5c238784837cb
DragonicNick <nickoorthuizen@gmail.com>
2018-10-23 14:56:13 +0200

    Player container&loader added

---
+++ Unity/Assets/Scripts/PlayerContainer.cs
@@ -0,0 +1,22 @@
+﻿using Api;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Container", menuName = "CardContainer")]
+public class PlayerContainer : ScriptableObject, ILoadable
+{
+	public List<Player> Players;
+
+	public void SetData<T>(T[] entities)
+	{
+		if (!(entities is Player[]))
+		{
+			Debug.Log("Data invalid");
+			return;
+		}
+
+		Players = (entities as Player[]).ToList();
+
+	}
+}
