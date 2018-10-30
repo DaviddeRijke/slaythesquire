7d0bb91ee745c16deefd4ebbaf457d2bb97ab764
Rupture13 <marco.nieuwenhuizen@student.fontys.nl>
2018-10-30 15:39:19 +0100

    Added EndOfMatch Screen

---
+++ Unity/Assets/EndOfMatch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndOfMatch : MonoBehaviour {
+    public GameObject[] objectsToHide;
+    public GameObject[] objectsToShow;
+    public GameObject winText;
+    public GameObject loseText;
+    public Text scoreAddition;
+    public Text scoreResult;
+    public Text goldAddition;
+    public Text goldResult;
+
+    public void EndMatch()
+    {
+        bool win = System.Convert.ToBoolean(Random.Range(0, 1));
+
+        ToggleUI(win);
+    }
+
+    private void ToggleUI(bool state)
+    {
+        foreach (GameObject obj in objectsToHide)
+        {
+            obj.SetActive(false);
+        }
+
+        foreach (GameObject obj in objectsToShow)
+        {
+            obj.SetActive(true);
+        }
+
+        if (state)
+        {
+            winText.SetActive(true);
+        }
+        else
+        {
+            loseText.SetActive(true);
+        }
+    }
+}
