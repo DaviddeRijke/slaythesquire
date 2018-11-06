c2535c4e010d85487c8e8acc8c651dedfe2a4aa6
Rupture13 <marco.nieuwenhuizen@student.fontys.nl>
2018-11-06 09:54:19 +0100

    MatchmakingLoadingScreen

---
+++ Unity/Assets/Imports/loadingBar/scripts/rotatetotate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class rotatetotate : MonoBehaviour {
+
+    private RectTransform rectComponent;
+    private Image imageComp;
+    private bool up = false;
+    private float filly;
+
+    public float rotateSpeed = 200f;
+
+    // Use this for initialization
+    void Start () {
+        rectComponent = GetComponent<RectTransform>();
+        imageComp = rectComponent.GetComponent<Image>();
+        filly = 1f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!up)
+        {
+            filly -= 0.05f;
+            if (filly <= 0)
+            {
+                imageComp.fillClockwise = !imageComp.fillClockwise;
+                up = true;
+            }
+        }
+        else
+        {
+            filly += 0.05f;
+            if (filly >= 1f)
+            {
+                imageComp.fillClockwise = !imageComp.fillClockwise;
+                up = false;
+            }
+        }
+
+        float currentSpeed = rotateSpeed * Time.deltaTime;
+        rectComponent.Rotate(0f, 0f, currentSpeed);
+
+        imageComp.fillAmount = filly;
+    }
+}
---
+++ Unity/Assets/Scripts/MatchMakerScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchMakerScreen : MonoBehaviour {
+    public GameObject[] objectsToHide;
+    public GameObject[] objectsToShow;
+
+    public void ToggleUI(bool showMatchmaking)
+    {
+        foreach (GameObject obj in objectsToHide)
+        {
+            obj.SetActive(!showMatchmaking);
+        }
+
+        foreach (GameObject obj in objectsToShow)
+        {
+            obj.SetActive(showMatchmaking);
+        }
+
+
+    }
+}
