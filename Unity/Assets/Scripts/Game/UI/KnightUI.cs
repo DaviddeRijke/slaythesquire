288a7e197a69d1507a677f6515dee279b40efee8
Rupture13 <marco.nieuwenhuizen@student.fontys.nl>
2018-11-27 15:55:51 +0100

    Added Equipment Slots Visuals

--- Unity/Assets/Scripts/Game/UI/KnightUI.cs
+++ Unity/Assets/Scripts/Game/UI/KnightUI.cs
@@ -12,6 +12,11 @@ public class KnightUI : MonoBehaviour {
     public Image healthBar;
     public Gradient healthTextColor;
     public Gradient healthBarColor;
+    public Button equipmentButton;
+    public GameObject equipmentPanel;
+    private Animator ani;
+    private bool collapse;
+
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +25,9 @@ public class KnightUI : MonoBehaviour {
 
         SetHealth(hp);
         initialHp = hp;
+
+        collapse = false;
+        ani = equipmentPanel.GetComponent<Animator>();
 	}
 
     public void SetName(string newName)
@@ -40,5 +48,20 @@ public class KnightUI : MonoBehaviour {
 
         healthText.color = healthTextColor.Evaluate(1 - relative);
         healthBar.color = healthBarColor.Evaluate(1 - relative);
+    }
+
+    public void ToggleEquipmentPanel()
+    {
+        if (collapse)
+        {
+            collapse = false;
+            equipmentButton.GetComponentInChildren<Text>().text = "Hide Equipment";
+        }
+        else
+        {
+            collapse = true;
+            equipmentButton.GetComponentInChildren<Text>().text = "Show Equipment";
+        }
+        ani.SetBool("Collapse", collapse);
     }
 }
