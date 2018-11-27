318aad7477f46d4260a326c0c186e1cc4696723a
DragonicNick <nickoorthuizen@gmail.com>
2018-11-27 12:39:06 +0100

    Equip effect & equipment added

--- Unity/Assets/Scripts/Effect/EffectHeal.cs
+++ Unity/Assets/Scripts/Effect/EffectHeal.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "EffectHeal", menuName = "ScriptableObjects/EffectHeal")]
+[CreateAssetMenu(fileName = "EffectHeal", menuName = "Effects/Heal")]
 public class EffectHeal : Effect {
 
 	public int amount;
 
 	public override void Activate(Knight self, Knight opponent)
 	{
-		self.hp += amount;
+		self.health += amount;
 	}
 }
--- Unity/Assets/Scripts/Game/Knight.cs
+++ Unity/Assets/Scripts/Game/Knight.cs
@@ -1,7 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Knight : MonoBehaviour {
-	public int hp;
+	public int health;
+	public List<Equipment> equipped;
+
+	public Knight()
+	{
+		equipped = new List<Equipment>();
+	}
+
+	public void Equip(Equipment equipment)
+	{
+		foreach (Equipment item in equipped)
+		{
+			if (item.equipmentSlot == equipment.equipmentSlot)
+			{
+				equipped.Remove(item);
+				break;
+			}
+		}
+		equipped.Add(equipment);
+	}
+
+	[System.Serializable]
+	public class ValueChanged : UnityEvent<int> { }
 }
