2a431d460b0aa78ad93042e5a5a1551fdcc1abba
BobvanVelzen <bob.vanvelzen@student.fontys.nl>
2018-11-27 11:44:55 +0100

    SocketService Added

---
+++ Unity/Assets/Scripts/WebSockets/SocketService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SocketService : MonoBehaviour {
+
+    public MessageHandler handler;
+
+    public UnityEvent<List<Card>, List<Card>> OnRecieve;
+
+    void Awake()
+    {
+        handler.Subscribe("", p => {
+            
+        });
+    }
+
+
+
+    void Recieve(List<Card> cards, List<Card> cards2)
+    {
+        OnRecieve.Invoke(cards, cards2);
+    }
+
+    void OnPlayCard(Card card)
+    {
+
+    }
+}
